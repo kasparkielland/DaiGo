49976090a221632516bea75955d2ad6c6ca3cbe3
kaspar <n10588281@qut.edu.au>
2019-09-05 13:39:43 +1000

    * App.xaml.cs: Changed MainPage = new ... for testing
    * DaiGo.csproj:
    * UserMainPage.xaml:
    * UserMainScreen.xaml:
    * HomeScreen_User.xaml:
    * UserMainPage.xaml.cs:
    * UserMainScreen.xaml.cs:
    * HomeScreen_User.xaml.cs:
    * HomeScreen_UserDetail.xaml:
    * HomeScreen_UserMaster.xaml:
    * HomeScreen_UserMenuItem.cs:
    * HomeScreen_UserDetail.xaml.cs:
    * HomeScreen_UserMaster.xaml.cs:

---
+++ DaiGo/View/HomeScreen_UserDetail.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DaiGo.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeScreen_UserDetail : ContentPage
+    {
+        public HomeScreen_UserDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
