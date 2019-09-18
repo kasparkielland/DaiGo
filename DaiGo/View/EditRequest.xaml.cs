4773a2126f6a945b6db6ef6935ad044c2a9d1ba1
kaspar <n10588281@qut.edu.au>
2019-09-18 16:42:34 +1000

    * DaiGo_Branding.png: modified icons
    * DaiGo.iOS.csproj:
    * EditRequest.xaml:
    * EditRequest.xaml.cs:
    * DaiGo_Logo.png:
    * DaiGo.Android.csproj:
    * DaiGo_Logo-40x40.png:
    * DaiGo_Logo-58x58.png:
    * DaiGo_Logo-60x60.png:
    * DaiGo_Logo-80x80.png:
    * DaiGo_Logo-87x87.png:
    * DaiGo_Logo-120x120.png:
    * DaiGo_Logo-180x180.png:
    * DaiGo_Logo-1024x1024.png:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * DaiGo_Logo.png:
    * Contents.json:
    * DaiGo_Logo-87x87.png:
    * DaiGo_Logo-80x80.png:
    * DaiGo_Logo-60x60.png:
    * DaiGo_Logo-40x40.png:
    * DaiGo_Logo-58x58.png:
    * DaiGo_Logo-87x87-1.png:
    * DaiGo_Logo-80x80-2.png:
    * DaiGo_Logo-80x80-1.png:
    * DaiGo_Logo-60x60-1.png:
    * DaiGo_Logo-58x58-2.png:
    * DaiGo_Logo-40x40-2.png:
    * DaiGo_Logo-180x180.png:
    * DaiGo_Logo-120x120.png:
    * DaiGo_Logo-40x40-3.png:
    * DaiGo_Logo-180x180-1.png:
    * DaiGo_Logo-120x120-1.png:
    * DaiGo_Logo-1024x1024.png:
    * DaiGo_Logo-120x120-2.png:
    * DaiGo_Logo-1024x1024-1.png:

---
+++ DaiGo/View/EditRequest.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DaiGo.View
+{
+    public partial class EditRequest : ContentPage
+    {
+        public EditRequest()
+        {
+            InitializeComponent();
+        }
+    }
+}
