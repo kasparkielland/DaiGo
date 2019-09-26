5c54b056d3d1a7073085fa51e2932103d2798c8c
Yaoching Hsu <n10249516@qut.edu.au>
2019-09-26 13:07:35 +1000

    Add the splash page.  Including adding a splash_screen.xml in Droid.drawable, add SplashActivity.cs under Droid, add MyTheme.splash under Droid.values

--- DaiGo.Android/MainActivity.cs
+++ DaiGo.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace DaiGo.Droid
 {
-    [Activity(Label = "DaiGo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "DaiGo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
