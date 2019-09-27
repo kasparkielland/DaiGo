3f8b84d0cdae9357a107198237f956edffe18f59
Kaspar Kielland <n10588281@qut.edu.au>
2019-09-27 13:23:58 +1000

    Added redefined pages

---
+++ DaiGo/View/RequestSent.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DaiGo.View
+{
+    public partial class RequestSent : ContentPage
+    {
+        public RequestSent()
+        {
+            InitializeComponent();
+        }
+    }
+}
