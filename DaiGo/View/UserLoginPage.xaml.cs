12cdb1775dd6dad9f7d4e134f59002f5e5268cf4
Kaspar Kielland <n10588281@qut.edu.au>
2019-10-01 00:34:17 +1000

    Added login handling, still needs to be connected to database

--- DaiGo/View/UserLoginPage.xaml.cs
+++ DaiGo/View/UserLoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using DaiGo.Views;
+﻿using DaiGo.ViewModels;
+using DaiGo.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +13,27 @@ namespace DaiGo.View
 {
     public partial class UserLoginPage : ContentPage
     {
+        public UserLoginViewModel userLoginViewModel;
+
         public UserLoginPage()
         {
             InitializeComponent();
+            userLoginViewModel = new UserLoginViewModel();
+            MessagingCenter.Subscribe<UserLoginViewModel, string>(this, "LoginAlert", (sender, username) =>
+            {
+                DisplayAlert("Title", username, "Okay");
+            });
+            this.BindingContext = userLoginViewModel;
+
+            usernameEntry.Completed += (object sender, EventArgs e) =>
+            {
+                passwordEntry.Focus();
+            };
+
+            passwordEntry.Completed += (object sender, EventArgs e) =>
+            {
+                userLoginViewModel.LoginCommand.Execute(null);
+            };
         }
 
         async void OnSignupButtonClicked(object sender, EventArgs args)
@@ -22,35 +41,12 @@ namespace DaiGo.View
             await Navigation.PushAsync(new UserSignUp());
         }
 
-        async void OnLoginButtonClicked(object sender, EventArgs args)
-        {
-            // Do login sequense
-
-            
-            // Go to UserMainPage
-            await Navigation.PushAsync(new UserMainPage());
-        }
-
         async void Button_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new ItemsPage());
         }
 
-        // Example to Login handling (https://docs.microsoft.com/nb-no/xamarin/xamarin-forms/app-fundamentals/navigation/hierarchical)
-        //async void OnLoginButtonClicked(object sender, EventArgs e)
-        //{
-        //    var isValid = AreCredentialsCorrect(user);
-        //    if (isValid)
-        //    {
-        //        App.IsUserLoggedIn = true;
-        //        Navigation.InsertPageBefore(new MainPage(), this);
-        //        await Navigation.PopAsync();
-        //    }
-        //    else
-        //    {
-        //        // Login failed
-        //    }
-        //}
+
 
     }
 }
---
+++ DaiGo/ViewModels/UserLoginViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DaiGo.ViewModels
+{
+    public class UserLoginViewModel : INotifyPropertyChanged
+    {
+        public UserLoginViewModel()
+        {
+            LoginCommand = new Command(OnLogin);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private void OnLogin()
+        {
+            if (string.IsNullOrEmpty(Username))
+            {
+                MessagingCenter.Send(this, "LoginAlert", Username);
+            }
+        }
+        public string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Username"));
+            }
+        }
+        public string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+        public ICommand LoginCommand { get; set; }
+    }
+
+    // Example to Login handling (https://docs.microsoft.com/nb-no/xamarin/xamarin-forms/app-fundamentals/navigation/hierarchical)
+    //async void OnLoginButtonClicked(object sender, EventArgs e)
+    //{
+    //    var isValid = AreCredentialsCorrect(user);
+    //    if (isValid)
+    //    {
+    //        App.IsUserLoggedIn = true;
+    //        Navigation.InsertPageBefore(new MainPage(), this);
+    //        await Navigation.PopAsync();
+    //    }
+    //    else
+    //    {
+    //        // Login failed
+    //    }
+    //}
+}
